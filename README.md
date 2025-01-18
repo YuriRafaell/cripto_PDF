7ff3eb2c597aa25ee92af4110982f101d32f1728
YuriRafaell <steffanyj4203@gmail.com>
2025-01-18 18:32:03 +0000

    finish the project

--- README.md
+++ README.md
@@ -9,39 +9,19 @@
 git clone (link do projeto)
 ```
 
-2. Coloque o arquivo PDF que deseja criptografar na pasta do projeto.
-
-3. Abra o VS Code manualmente ou via CMD (no CMD, vá até o diretório do projeto):
+2. Abra o VS Code manualmente ou via CMD (no CMD, vá até o diretório do projeto):
 
 ```
 code .
 ```
 
-4. Renomeie os documentos no código:
-
-```
-caminho_pdf = 'teste.pdf'
-caminho_pdf_protegido = 'documento_protegido.pdf'
-senha = 'sua_senha'
-```
-
-5. No terminal do VS Code ou no seu próprio CMD, execute:
+3. No terminal do VS Code ou no seu próprio CMD, execute:
 
 ```
 python3 app.py
 ```
 
 OBS: Rode o python de acordo com sua versão instalada no seu sistema.
-
-# Explicando um pouco do código
-
-1. O que é buffer?
- * É uma área de memória temporária usada para armazenar dados enquanto eles são transferidos de um local para outro. O buffer é usado para criar um PDF na memória antes de gravá-lo em um arquivo.
- ### Vantagens do Uso de Buffer
-  * Eficiência: Operações de leitura e gravação em memória são geralmente mais rápidas do que    operações de disco.
-  * Flexibilidade: Permite manipular os dados antes de gravá-los no disco, como aplicar criptografia ou compressão.
-  * Segurança: Dados sensíveis podem ser mantidos na memória temporariamente e manipulados antes de serem escritos em um local permanente.
-
 
 ### Para tornar um executavel 
 
@@ -54,3 +34,5 @@ pip install pyinstaller
 ```
 pyinstaller --onefile app.py
 ```
+
+Vai gerar duas pastas: `build`e `dist`. Dentro da pasta `dist` estará o executavel.
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from PyPDF2 import PdfWriter, PdfReader
 
+# Função para proteger arquivos PDF
 def proteger_pdf(arquivo_pdf, arquivo_pdf_protegido, senha):
     pdf_writer = PdfWriter()
     pdf_reader = PdfReader(arquivo_pdf)
@@ -14,31 +15,43 @@ def proteger_pdf(arquivo_pdf, arquivo_pdf_protegido, senha):
     with open(arquivo_pdf_protegido, 'wb') as f:
         pdf_writer.write(f)
 
-    messagebox.showinfo("Sucesso", f"O PDF {arquivo_pdf_protegido} foi criado e protegido com senha.")
-
+# Função para escolher arquivo
 def selecionar_arquivo():
-    arquivo_pdf = filedialog.askopenfilename(title="Selecione o arquivo PDF",
-                                             filetypes=[("PDF files", "*.pdf")])
-    if arquivo_pdf:
-        entry_arquivo_pdf.delete(0, tk.END)
-        entry_arquivo_pdf.insert(0, arquivo_pdf)
+    arquivo = filedialog.askopenfilename(
+        title="Selecione o arquivo",
+        filetypes=[
+            ("Arquivos PDF", "*.pdf")
+        ]
+    )
+    if arquivo:
+        entry_arquivo.delete(0, tk.END)
+        entry_arquivo.insert(0, arquivo)
 
 def processar():
-    arquivo_pdf = entry_arquivo_pdf.get()
+    arquivo = entry_arquivo.get()
     nome_do_doc = entry_nome_do_doc.get()
     senha = entry_senha.get()
 
-    if not arquivo_pdf or not nome_do_doc or not senha:
+    if not arquivo or not nome_do_doc or not senha:
         messagebox.showerror("Erro", "Todos os campos são obrigatórios!")
         return
 
-    arquivo_pdf_protegido = f"{nome_do_doc}.pdf"
-    proteger_pdf(arquivo_pdf, arquivo_pdf_protegido, senha)
+    try:
+        extensao = arquivo.lower().split('.')[-1]
+        arquivo_protegido = f"{nome_do_doc}.{extensao}"
+
+        if extensao == 'pdf':
+            proteger_pdf(arquivo, arquivo_protegido, senha)
+            messagebox.showinfo("Sucesso", f"O PDF foi protegido com senha: {arquivo_protegido}")
+        else:
+            messagebox.showerror("Erro", "Formato de arquivo não suportado!")
+    except Exception as e:
+        messagebox.showerror("Erro", f"Erro ao processar o arquivo: {str(e)}")
 
 root = tk.Tk()
-root.title("Protegendo seu documento")
+root.title("Protegendo seu PDF")
 
-# Definindo altura e largura da janela
+# Configuração da janela
 window_width = 1400
 window_height = 600
 screen_width = root.winfo_screenwidth()
@@ -47,43 +60,38 @@ position_top = int(screen_height/2 - window_height/2)
 position_right = int(screen_width/2 - window_width/2)
 root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
 
-# Add a header
-header = tk.Label(root, text="Protegendo seu documento", fg='white', font=('Arial', 20, 'bold'))
-header.pack(fill=tk.X, pady=10)
+# Cabeçalho
+header = tk.Label(root, text="Protegendo seu documento PDF", fg='black', font=('Arial', 30, 'bold'))
+header.pack(fill=tk.X, pady=50)
 
-# Frame for form
+# Frame para o formulário
 frame = tk.Frame(root, bg='#f0f0f0')
 frame.pack(pady=20, padx=20)
 
-# Nome do Arquivo PDF
-label_arquivo_pdf = tk.Label(frame, text="Arquivo PDF:", bg='#f0f0f0', font=('Arial', 12))
-label_arquivo_pdf.grid(row=0, column=0, padx=10, pady=5, sticky=tk.E)
-entry_arquivo_pdf = tk.Entry(frame, width=40, font=('Arial', 12))
-entry_arquivo_pdf.grid(row=0, column=1, padx=10, pady=5)
+# Arquivo
+label_arquivo = tk.Label(frame, text="Arquivo:", bg='#f0f0f0', font=('Arial', 15))
+label_arquivo.grid(row=0, column=0, padx=10, pady=5, sticky=tk.E)
+entry_arquivo = tk.Entry(frame, width=40, font=('Arial', 12))
+entry_arquivo.grid(row=0, column=1, padx=10, pady=5)
 
 # Botão Selecionar
 btn_selecionar = tk.Button(frame, text="Selecionar", command=selecionar_arquivo, font=('Arial', 12), bg='#4a90e2', fg='white')
 btn_selecionar.grid(row=0, column=2, padx=10, pady=5)
 
 # Nome do Arquivo Protegido
-label_nome_do_doc = tk.Label(frame, text="Nome do Arquivo Protegido:", bg='#f0f0f0', font=('Arial', 12))
+label_nome_do_doc = tk.Label(frame, text="Nome do Arquivo Protegido:", bg='#f0f0f0', font=('Arial', 15))
 label_nome_do_doc.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
 entry_nome_do_doc = tk.Entry(frame, width=40, font=('Arial', 12))
 entry_nome_do_doc.grid(row=1, column=1, padx=10, pady=5, columnspan=2)
 
 # Senha
-label_senha = tk.Label(frame, text="Senha:", bg='#f0f0f0', font=('Arial', 12))
+label_senha = tk.Label(frame, text="Senha:", bg='#f0f0f0', font=('Arial', 15))
 label_senha.grid(row=2, column=0, padx=10, pady=5, sticky=tk.E)
 entry_senha = tk.Entry(frame, show='*', width=40, font=('Arial', 12))
 entry_senha.grid(row=2, column=1, padx=10, pady=5, columnspan=2)
 
 # Botão Processar
-btn_processar = tk.Button(root, text="Proteger PDF", command=processar, font=('Arial', 12, 'bold'), bg='#4a90e2', fg='white')
+btn_processar = tk.Button(root, text="Proteger PDF", command=processar, font=('Arial', 15, 'bold'), bg='#4a90e2', fg='white')
 btn_processar.pack(pady=20)
-
-# Centralizar widgets verticalmente
-frame.grid_rowconfigure(3, weight=1)
-frame.grid_columnconfigure(0, weight=1)
-frame.grid_columnconfigure(3, weight=1)
 
 root.mainloop()
